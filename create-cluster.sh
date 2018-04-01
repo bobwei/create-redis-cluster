b7b76aa8b7832eb92c4cc1855449b62f6fe1594b
Bob Wei <bob.wei@linecorp.com>
2018-04-01 22:02:39 +0800

    Refactor create script, add NUMBER_OF_NODES

--- create-cluster.sh
+++ create-cluster.sh
@@ -1,14 +1,17 @@
 export $(cat .env | xargs)
 
-node_addresses="$(for index in `seq 1 6`; do \
-echo "$(docker inspect -f \
-"{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}" \
-"${PROJECT_NAME}_${SERVICE_NAME}_$index"):6379"
-done)"
+for index in `seq 1 $NUMBER_OF_NODES`; do \
+  node_addresses="$(docker inspect -f \
+  "{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}" \
+  "${PROJECT_NAME}_${SERVICE_NAME}_$index"):6379 $node_addresses"
+done
 
-echo $node_addresses
+echo "$node_addresses \n"
 
-docker run -i --rm --net $NETWORK_NAME ruby sh -c '\
- gem install redis \
- && wget http://download.redis.io/redis-stable/src/redis-trib.rb \
- && ruby redis-trib.rb create --replicas 1 "$node_addresses"'
+cmd=$(echo "\
+  gem install redis \
+  && wget http://download.redis.io/redis-stable/src/redis-trib.rb \
+  && ruby redis-trib.rb create --replicas 1 ${node_addresses} \
+")
+
+docker run -i --rm --net $NETWORK_NAME ruby sh -c "$cmd"
